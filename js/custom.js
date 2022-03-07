cff7ebc061dffcd85beb6472ecbbc87804018b24
Ayan Das <ayandas630@gmail.com>
2022-03-07 11:01:34 +0530

    complete

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+var navLinks = document.getElementById("navLinks");
+function showmenu() {
+  console.log('showMenu');
+  navLinks.style.right = "0";
+}
+function hidemenu() {
+  console.log('hideMenu');
+  navLinks.style.right = "-500px";
+}
+
