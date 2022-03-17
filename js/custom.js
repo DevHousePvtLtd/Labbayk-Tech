59b2d3e588284d09f49f8e357043f21e261eb186
Ayan Das <ayandas630@gmail.com>
2022-03-18 02:35:26 +0530

    contact us page done

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,30 @@ function hidemenu() {
   navLinks.style.right = "-500px";
 }
 
+$('.floating-label').on({
+  'focus': function(){
+    if( $(this).val() === "" ){
+      $(this).parent().find('label').addClass('field-active');
+    }
+    if( $(this).val()==="" ){
+      $(this).parent().find('label').addClass('field-active');
+    }
+  },
+  'blur': function(){
+    if( $(this).val() === "" ){
+      $(this).parent().find('label').removeClass('field-active');
+    }
+  }
+});
+$('.select').on({
+  'click': function(){
+   
+      $(this).parent().find('label').addClass('field-active');
+    
+  },
+  'blur': function(){
+    if( $(this).val() === "" ){
+      $(this).parent().find('label').removeClass('field-active');
+    }
+  }
+});
