37df839a05f015c62143ad6161f04623e629ca35
Ayan Das <54409741+expertad630@users.noreply.github.com>
2022-04-20 21:59:05 +0530

    modal added in portfilo page
    added in the first image

--- js/custom.js
+++ js/custom.js
@@ -8,30 +8,40 @@ function hidemenu() {
   navLinks.style.right = "-500px";
 }
 
-$('.floating-label').on({
-  'focus': function(){
-    if( $(this).val() === "" ){
-      $(this).parent().find('label').addClass('field-active');
-    }
-    if( $(this).val()==="" ){
-      $(this).parent().find('label').addClass('field-active');
-    }
-  },
-  'blur': function(){
-    if( $(this).val() === "" ){
-      $(this).parent().find('label').removeClass('field-active');
-    }
-  }
-});
-$('.select').on({
-  'click': function(){
+// $('.floating-label').on({
+//   'focus': function(){
+//     if( $(this).val() === "" ){
+//       $(this).parent().find('label').addClass('field-active');
+//     }
+//     if( $(this).val()==="" ){
+//       $(this).parent().find('label').addClass('field-active');
+//     }
+//   },
+//   'blur': function(){
+//     if( $(this).val() === "" ){
+//       $(this).parent().find('label').removeClass('field-active');
+//     }
+//   }
+// });
+// $('.select').on({
+//   'click': function(){
    
-      $(this).parent().find('label').addClass('field-active');
+//       $(this).parent().find('label').addClass('field-active');
     
-  },
-  'blur': function(){
-    if( $(this).val() === "" ){
-      $(this).parent().find('label').removeClass('field-active');
-    }
-  }
-});
+//   },
+//   'blur': function(){
+//     if( $(this).val() === "" ){
+//       $(this).parent().find('label').removeClass('field-active');
+//     }
+//   }
+// });
+
+
+function openModal(params) {
+  document.getElementById(params).style.display='flex';
+  document.getElementById(params).style.opacity=1;
+}
+function closeModal(params) {
+  document.getElementById(params).style.display='none';
+  document.getElementById(params).style.opacity=0;
+}
